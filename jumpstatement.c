779aa9f0e4683e2d095e04e9177d62c4f24fcbc7
Nikhil Tamatta <31841197+ntamatta@users.noreply.github.com>
2017-09-25 11:57:58 +0530

    Add files via upload

---
+++ jumpstatement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("Enter the number :");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("It is Even Number");
+    else
+    printf("It is Odd Number");
+    getch();
+}
