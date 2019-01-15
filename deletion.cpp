be058a94faccd4f9bcb6786cfc706bb358024fd1
Nikhil Tamatta <31841197+ntamatta@users.noreply.github.com>
2019-01-15 11:39:18 +0530

    Add files via upload

---
+++ deletion.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,m,v;
+    cout<<"Enter the number of elements:-";
+    cin>>n;
+    int a[n+1];
+    cout<<"\nEnter elements:-";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"n="<<n;
+    cout<<"\nElements:-";
+    for(i=0;i<n;i++)
+    {
+        cout<<"\n"<<a[i];
+    }
+    cout<<"\nEnter the value to be deleted:-";
+    cin>>v;
+    for(i=0;i<n;i++)
+    {
+        if(v==a[i])
+        {
+            m=i;
+        }
+    }
+    for(i=m;i<n;i++)
+    {
+        a[i]=a[i+1];
+    }
+    for(i=0;i<(n-1);i++)
+    {
+        cout<<"\n"<<a[i];
+    }
+}
+
